ada7aa158a4fb515e750a592b6a894b6dbe51869
anderal29 <anderal@whs.school.nz>
2023-06-01 12:52:13 +1200

    first version of map generation

---
+++ SweepingMines_V1_MapGen/main.java
@@ -0,0 +1,87 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main
+{
+   public int mineCount;
+   public char map[][];
+   public char playersMap[][];
+    /**
+     * Constructor for objects of class main
+     */
+    public main()
+    {
+        map = new char[18][14];
+        playersMap = new char[18][14];
+        generateMap(4,4);
+        drawMap();
+        
+        
+    }
+    
+    public void drawMap(){
+        for(int y=0;y<14;y++){
+            for(int x=0;x<18;x++){
+                System.out.print(map[x][y]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+        
+
+    }
+    public void generateMap(int xStart,int yStart)
+    {   
+        //generate empty map with no mines
+        //also generates a seperate map, just for the player to see.
+        for(int x=0;x<18;x++){
+           for(int y=0;y<14;y++){
+               map[x][y] = '■';
+               playersMap[x][y] = '■';
+           }
+        }
+        
+        //generates 32 mines in random positions
+        //avoids generating mines where player clicked, or adjacent to where
+        //player clicked.
+        
+        int randomY;
+        int randomX;
+        System.out.println("clicked at: "+xStart+","+yStart);
+        for(int minesUnplaced=32;minesUnplaced>0;minesUnplaced--){
+            randomX = (int)Math.floor(Math.random()*18);
+            randomY = (int)Math.floor(Math.random()*14);
+            if((map[randomX][randomY] == '■')&&(!isAdjacent(xStart,yStart,randomX,randomY))){
+                map[randomX][randomY] = 'x';
+                //System.out.println("Mine generated at: "+randomX+","+randomY);
+            }else{
+                minesUnplaced++;
+                //System.out.println("Could not generate mine at "+randomX+","+randomY);
+            }
+            
+        }
+        
+    }
+    
+    public boolean isAdjacent(int x,int y,int targetX,int targetY){
+       
+        
+        for(int i=x-1 ; i<=x+1 ; i++){
+            for(int t=y-1 ; t<=y+1 ; t++){
+              
+               if((targetX == i)&&(targetY == t)){
+                   System.out.println("Checking square ("+i+","+t+") against mine ("+targetX+","+targetY+")"); 
+                   System.out.println("found adjacent mine");
+                   return true;
+                   
+               }
+            }
+        }
+        return false;
+    }
+}//(randomX != xStart)&&(randomY != yStart)
